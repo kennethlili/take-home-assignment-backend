cafb84586d5e457b7ce2e40a7c9feb61dedd2521
kennethlili <lichikin1997@gmail.com>
2025-04-28 16:09:18 -0400

    fix: PropertyRepository Bean Not Found Error

--- src/main/java/com/spatiallaser/backend/repository/PropertyRepository.java
+++ src/main/java/com/spatiallaser/backend/repository/PropertyRepository.java
@@ -3,8 +3,8 @@ package com.spatiallaser.backend.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import com.spatiallaser.backend.entity.Property;
 
@@ -13,14 +13,14 @@ import com.spatiallaser.backend.entity.Property;
  * https://www.e-education.psu.edu/spatialdb/node/1974
  */
 
-@Repository
-public interface PropertyRepository {
-    @Query(value = "SELECT p FROM Property p WHERE " +
-            "ST_Intersects(p.geom, ST_MakeEnvelope(:minLon, :minLat, :maxLon, :maxLat, 4326)) = true")
-    List<Property> findByBoundingBox(
-            @Param("minLon") double minLon,
-            @Param("minLat") double minLat,
-            @Param("maxLon") double maxLon,
-            @Param("maxLat") double maxLat);
+@org.springframework.stereotype.Repository
+public interface PropertyRepository extends Repository<Property, Long> {
+        @Query(value = "SELECT p FROM Property p WHERE " +
+                        "ST_Intersects(p.geom, ST_MakeEnvelope(:minLon, :minLat, :maxLon, :maxLat, 4326)) = true")
+        List<Property> findByBoundingBox(
+                        @Param("minLon") double minLon,
+                        @Param("minLat") double minLat,
+                        @Param("maxLon") double maxLon,
+                        @Param("maxLat") double maxLat);
 
 }
