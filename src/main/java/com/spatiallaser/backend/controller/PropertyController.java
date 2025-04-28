7a5fd8b9e688522c6623dae997b9b67636a4bc8a
kennethlili <lichikin1997@gmail.com>
2025-04-28 17:30:34 -0400

    feat: add CORS configuration to PropertyController

--- src/main/java/com/spatiallaser/backend/controller/PropertyController.java
+++ src/main/java/com/spatiallaser/backend/controller/PropertyController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,8 @@ import com.spatiallaser.backend.entity.Property;
 import com.spatiallaser.backend.repository.PropertyRepository;
 
 @RestController
+// TODO: add CORS configuration
+@CrossOrigin(origins = "*")
 @RequestMapping("/api")
 public class PropertyController {
 
