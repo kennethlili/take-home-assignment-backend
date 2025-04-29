5f9661267ac5d8c72bbd98eb7b98cdf39b7d7913
kennethlili <lichikin1997@gmail.com>
2025-04-29 17:49:56 -0400

    fix: add EnumType annotation for zoningType in Property entity

--- src/main/java/com/spatiallaser/backend/entity/reading/Property.java
+++ src/main/java/com/spatiallaser/backend/entity/reading/Property.java
@@ -6,6 +6,8 @@ import com.spatiallaser.backend.enums.ZoningTypeEnum;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -52,6 +54,7 @@ public class Property {
     private String zoningSub;
 
     @NotNull()
+    @Enumerated(EnumType.STRING)
     @Column(name = "zoning_typ")
     private ZoningTypeEnum zoningType;
 
